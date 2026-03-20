6cb1fc2c28abc969a6630858980f94cf2438f21e
Luis Tejón <l.tejon@uniandes.edu.co>
2026-03-20 17:58:54 -0500

    mejoras en tiempo de computacion

--- metodos_estadisticos.py
+++ metodos_estadisticos.py
@@ -15,35 +15,37 @@ def test_wilcoxon(x, y):
 
 
 def test_hodges_lehmann(x, y):
-    diffs = (y[:, None] - x).flatten()
-    diffs.sort()
-
     m, n = len(x), len(y)
-    mean_u = m * n / 2
-    std_u = np.sqrt(m * n * (m + n + 1) / 12)
+    mn = m * n
+
+    mean_u = mn / 2
+    std_u = np.sqrt(mn * (m + n + 1) / 12)
     z_alpha = stats.norm.ppf(1 - ALPHA / 2)
 
-    lower_idx = int(np.round(mean_u - z_alpha * std_u))
-    upper_idx = int(np.round(mean_u + z_alpha * std_u))
+    lower_idx = max(0,      int(np.round(mean_u - z_alpha * std_u)) - 1)
+    upper_idx = min(mn - 1, int(np.round(mean_u + z_alpha * std_u)) - 1)
 
-    lower_idx = max(0, lower_idx - 1)
-    upper_idx = min(len(diffs) - 1, upper_idx - 1)
+    diffs = (y[:, None] - x).flatten()
 
-    ci_lower = diffs[lower_idx]
-    ci_upper = diffs[upper_idx]
+    # partition garantiza que el elemento en lower_idx es el correcto
+    # y todo lo que está a su izquierda es <= que él (sin ordenar ese lado)
+    partitioned = np.partition(diffs, [lower_idx, upper_idx])
+
+    ci_lower = partitioned[lower_idx]
+    ci_upper = partitioned[upper_idx]
 
     return not (ci_lower <= 0 <= ci_upper)
 
 
 def test_permutacion(x, y, estadistico='media'):
-    def diff_medias(a, b):
-        return np.mean(a) - np.mean(b)
+    def diff_medias(a, b, axis=0):
+        return np.mean(a, axis=axis) - np.mean(b, axis=axis)
 
-    def diff_medianas(a, b):
-        return np.median(a) - np.median(b)
+    def diff_medianas(a, b, axis=0):
+        return np.median(a, axis=axis) - np.median(b, axis=axis)
 
-    def diff_trim(a, b):
-        return stats.trim_mean(a, 0.1) - stats.trim_mean(b, 0.1)
+    def diff_trim(a, b, axis=0):
+        return stats.trim_mean(a, 0.1, axis=axis) - stats.trim_mean(b, 0.1, axis=axis)
 
     if estadistico == 'media':
         stat_func = diff_medias
@@ -53,5 +55,5 @@ def test_permutacion(x, y, estadistico='media'):
         stat_func = diff_trim
 
     res = stats.permutation_test((x, y), stat_func, permutation_type='independent',
-                                 n_resamples=PERMUTATIONS, alternative='two-sided')
+                                 n_resamples=PERMUTATIONS, alternative='two-sided', vectorized=True)
     return res.pvalue < ALPHA
